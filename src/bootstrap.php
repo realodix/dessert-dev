7dd4257b679d3e7ddc1c3625655c50a604caaabc
Kei <budi03@gmail.com>
2021-07-07 20:49:34 +0700

    Update bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -15,18 +15,6 @@ if (! function_exists('verify')) {
     }
 }
 
-if (! function_exists('verify_that')) {
-    /**
-     * @param mixed $actual
-     *
-     * @return VerifyAny
-     */
-    function verify_that($actual): VerifyAny
-    {
-        return new VerifyAny($actual);
-    }
-}
-
 if (! function_exists('expect')) {
     /**
      * @param mixed $actual
