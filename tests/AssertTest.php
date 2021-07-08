d8239fc7bae71a60bd05772295fae4713737fcb4
Kei <budi03@gmail.com>
2021-07-08 15:26:53 +0700

    Rename to AssertTest

--- tests/AssertTest.php
+++ tests/AssertTest.php
@@ -6,7 +6,7 @@ use Exception;
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
 
-final class VerifyTest extends TestCase
+final class AssertTest extends TestCase
 {
     /** @var DOMDocument */
     protected $xml;
