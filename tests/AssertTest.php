f7909f5ca971ae3619576b7547a49865049eabf5
Kei <budi03@gmail.com>
2021-07-11 00:51:56 +0700

    Update AssertTest.php

--- tests/AssertTest.php
+++ tests/AssertTest.php
@@ -17,6 +17,24 @@ final class AssertTest extends TestCase
         $this->assetsDir = __DIR__.DIRECTORY_SEPARATOR.'_files'.DIRECTORY_SEPARATOR;
     }
 
+    public function testClassHasAttribute(): void
+    {
+        ass('Exception')->classHasAttribute('message');
+        ass('Exception')->classNotHasAttribute('fakeproperty');
+
+        if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
+            $testObject = (object) ['existingAttribute' => true];
+            ass($testObject)->objectNotHasAttribute('fakeproperty');
+            ass($testObject)->objectHasAttribute('existingAttribute');
+        }
+    }
+
+    public function testClassHasStaticAttribute(): void
+    {
+        ass(FakeClassForTesting::class)->classHasStaticAttribute('staticProperty');
+        ass(FakeClassForTesting::class)->classNotHasStaticAttribute('fakeProperty');
+    }
+
     public function testEmptyNotEmpty(): void
     {
         ass(['3', '5'])->notEmpty();
@@ -28,62 +46,26 @@ final class AssertTest extends TestCase
         ass(5)->equals(5);
         ass('hello')->equals('hello');
         ass(5)->equals(5, 'user have 5 posts');
-        ass(3.251)->equalsWithDelta(3.25, 0.01);
-        ass(3.251)->equalsWithDelta(3.25, 0.01, 'respects delta');
+
+        ass(3)->notEquals(5);
     }
 
     public function testEqualsCanonicalizing(): void
     {
         ass([3, 2, 1])->equalsCanonicalizing([1, 2, 3]);
-    }
 
-    public function testEqualsFile(): void
-    {
-        ass('testing 123')->stringEqualsFile($this->assetsDir.'StringEqualsFile.txt');
-        ass('Another string')->stringNotEqualsFile($this->assetsDir.'StringEqualsFile.txt');
-
-        ass('testing 123')
-            ->stringEqualsFileCanonicalizing($this->assetsDir.'StringEqualsFile.txt');
-        ass('notSame')
-            ->stringNotEqualsFileCanonicalizing($this->assetsDir.'StringEqualsFile.txt');
-        ass('TESTING 123')
-            ->stringEqualsFileIgnoringCase($this->assetsDir.'StringEqualsFile.txt');
-        ass('Test 123')
-            ->stringNotEqualsFileIgnoringCase($this->assetsDir.'StringEqualsFile.txt');
-    }
-
-    public function testEqualsJsonFile(): void
-    {
-        $jsonFile = $this->assetsDir.'JsonData/simpleObject.json';
-
-        ass($jsonFile)->jsonFileEqualsJsonFile($jsonFile);
-        ass('{"Mascott":"Tux"}')->jsonStringEqualsJsonFile($jsonFile);
-    }
-
-    public function testEqualsJsonString(): void
-    {
-        $jsonString = '{"foo" : "bar"}';
-        ass($jsonString)->jsonStringEqualsJsonString($jsonString);
+        ass([3, 2, 1])->notEqualsCanonicalizing([2, 3, 0, 1]);
     }
 
     public function testEqualsWithDelta(): void
     {
         ass(1.01)->equalsWithDelta(1.0, 0.1);
-    }
+        ass(3.251)->equalsWithDelta(3.25, 0.01);
+        ass(3.251)->equalsWithDelta(3.25, 0.01, 'respects delta');
 
-    public function testEqualsXmlFile(): void
-    {
-        $xmlFile = $this->assetsDir.'xml-test-file.xml';
-
-        ass($xmlFile)->xmlFileEqualsXmlFile($xmlFile);
-
-        ass('<foo><bar>Baz</bar><bar>Baz</bar></foo>')->xmlStringEqualsXmlFile($xmlFile);
-    }
-
-    public function testEqualsXmlString(): void
-    {
-        ass('<foo><bar>Baz</bar><bar>Baz</bar></foo>')
-            ->xmlStringEqualsXmlString('<foo><bar>Baz</bar><bar>Baz</bar></foo>');
+        ass(1.2)->notEqualsWithDelta(1.0, 0.1);
+        ass(3.252)->notEqualsWithDelta(3.25, 0.001);
+        ass(3.252)->notEqualsWithDelta(3.25, 0.001, 'respects delta');
     }
 
     public function testFinite(): void
@@ -99,24 +81,6 @@ final class AssertTest extends TestCase
         ass(7)->lessThanOrEqual(8);
         ass(7)->greaterThanOrEqual(7);
         ass(7)->greaterThanOrEqual(5);
-    }
-
-    public function testHasAttribute(): void
-    {
-        ass('Exception')->classHasAttribute('message');
-        ass('Exception')->classNotHasAttribute('fakeproperty');
-
-        if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
-            $testObject = (object) ['existingAttribute' => true];
-            ass($testObject)->objectNotHasAttribute('fakeproperty');
-            ass($testObject)->objectHasAttribute('existingAttribute');
-        }
-    }
-
-    public function testHasStaticAttribute(): void
-    {
-        ass(FakeClassForTesting::class)->classHasStaticAttribute('staticProperty');
-        ass(FakeClassForTesting::class)->classNotHasStaticAttribute('fakeProperty');
     }
 
     public function testInfinite(): void
@@ -136,34 +100,23 @@ final class AssertTest extends TestCase
         ass(json_encode(['foo' => 'bar']))->json();
     }
 
-    public function testMatchesFormatFile(): void
+    public function testJsonEqualsJsonFile(): void
     {
-        $formatFile = $this->assetsDir.'StringEqualsFile.txt';
+        $jsonFile = $this->assetsDir.'JsonData/simpleObject.json';
 
-        ass('testing 123')->stringMatchesFormatFile($formatFile);
-        ass('asdfas')->stringNotMatchesFormatFile($formatFile);
+        ass($jsonFile)->jsonFileEqualsJsonFile($jsonFile);
+        ass('{"Mascott":"Tux"}')->jsonStringEqualsJsonFile($jsonFile);
+    }
+
+    public function testJsonStringEqualsJsonString(): void
+    {
+        $jsonString = '{"foo" : "bar"}';
+        ass($jsonString)->jsonStringEqualsJsonString($jsonString);
     }
 
     public function testNan(): void
     {
         ass(NAN)->nan();
-    }
-
-    public function testNotEquals(): void
-    {
-        ass(3)->notEquals(5);
-        ass(3.252)->notEqualsWithDelta(3.25, 0.001);
-        ass(3.252)->notEqualsWithDelta(3.25, 0.001, 'respects delta');
-    }
-
-    public function testNotEqualsCanonicalizing(): void
-    {
-        ass([3, 2, 1])->notEqualsCanonicalizing([2, 3, 0, 1]);
-    }
-
-    public function testNotEqualsWithDelta(): void
-    {
-        ass(1.2)->notEqualsWithDelta(1.0, 0.1);
     }
 
     public function testSame(): void
@@ -172,11 +125,56 @@ final class AssertTest extends TestCase
         ass(1)->notSame(true);
     }
 
+    public function testStringEqualsFile(): void
+    {
+        ass('testing 123')->stringEqualsFile($this->assetsDir.'StringEqualsFile.txt');
+        ass('Another string')->stringNotEqualsFile($this->assetsDir.'StringEqualsFile.txt');
+    }
+
+    public function testStringEqualsFileCanonicalizing(): void
+    {
+        ass('testing 123')
+            ->stringEqualsFileCanonicalizing($this->assetsDir.'StringEqualsFile.txt');
+        ass('notSame')
+            ->stringNotEqualsFileCanonicalizing($this->assetsDir.'StringEqualsFile.txt');
+    }
+
+    public function testStringEqualsFileIgnoringCase(): void
+    {
+        ass('TESTING 123')
+            ->stringEqualsFileIgnoringCase($this->assetsDir.'StringEqualsFile.txt');
+        ass('Test 123')
+            ->stringNotEqualsFileIgnoringCase($this->assetsDir.'StringEqualsFile.txt');
+    }
+
+    public function testStringMatchesFormatFile(): void
+    {
+        $formatFile = $this->assetsDir.'StringEqualsFile.txt';
+
+        ass('testing 123')->stringMatchesFormatFile($formatFile);
+        ass('asdfas')->stringNotMatchesFormatFile($formatFile);
+    }
+
     public function testVariants(): void
     {
         expect([])->empty();
         should([])->empty();
         verify([])->empty();
+    }
+
+    public function testXmlFileEqualsXmlFile(): void
+    {
+        $xmlFile = $this->assetsDir.'xml-test-file.xml';
+
+        ass($xmlFile)->xmlFileEqualsXmlFile($xmlFile);
+
+        ass('<foo><bar>Baz</bar><bar>Baz</bar></foo>')->xmlStringEqualsXmlFile($xmlFile);
+    }
+
+    public function testXmlStringEqualsXmlString(): void
+    {
+        ass('<foo><bar>Baz</bar><bar>Baz</bar></foo>')
+            ->xmlStringEqualsXmlString('<foo><bar>Baz</bar><bar>Baz</bar></foo>');
     }
 }
 
