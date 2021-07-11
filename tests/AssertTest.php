5a5dfe99c19dfae895f9a463514a47200ab26f62
Kei <budi03@gmail.com>
2021-07-11 18:32:38 +0700

    Pisahkan test XML

--- tests/AssertTest.php
+++ tests/AssertTest.php
@@ -161,48 +161,6 @@ final class AssertTest extends TestCase
         should([])->empty();
         verify([])->empty();
     }
-
-    public function testXmlFileEqualsXmlFile(): void
-    {
-        $xmlFile = $this->assetsDir.'xml-foo.xml';
-
-        ass($xmlFile)->xmlFileEqualsXmlFile($xmlFile);
-    }
-
-    public function testXmlFileNotEqualsXmlFile(): void
-    {
-        $actual = $this->assetsDir.'xml-foo.xml';
-        $expected = $this->assetsDir.'xml-bar.xml';
-
-        ass($actual)->xmlFileNotEqualsXmlFile($expected);
-    }
-
-    public function testXmlStringEqualsXmlFile(): void
-    {
-        $xmlFile = $this->assetsDir.'xml-foo.xml';
-
-        ass('<foo/>')->xmlStringEqualsXmlFile($xmlFile);
-    }
-
-    public function testXmlStringEqualsXmlString(): void
-    {
-        ass('<foo/>')
-            ->xmlStringEqualsXmlString('<foo/>');
-    }
-
-    public function testXmlStringNotEqualsXmlFile(): void
-    {
-        $actual = file_get_contents($this->assetsDir.'xml-foo.xml');
-        $expected = $this->assetsDir.'xml-bar.xml';
-
-        ass($actual)->xmlStringNotEqualsXmlFile($expected);
-    }
-
-    public function testXmlStringNotEqualsXmlString(): void
-    {
-        ass('<foo/>')
-            ->xmlStringNotEqualsXmlString('<bar/>');
-    }
 }
 
 class FakeClassForTesting
