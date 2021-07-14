aec96f80e7c591bc7da6adddaa7f2d6f15f35ca7
Kei <budi03@gmail.com>
2021-07-14 12:52:23 +0700

    formatting

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-if (!defined('TEST_FILES_PATH')) {
-    define('TEST_FILES_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR);
+if (! defined('TEST_FILES_PATH')) {
+    define('TEST_FILES_PATH', __DIR__.DIRECTORY_SEPARATOR.'_files'.DIRECTORY_SEPARATOR);
 }
