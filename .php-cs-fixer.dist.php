bbf03833ee439e5bad830aafe9da1379c7277455
Kei <budi03@gmail.com>
2021-07-16 00:58:08 +0700

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,7 @@ use Realodix\CsConfig\Factory;
 use Realodix\CsConfig\RuleSet;
 
 $overrideRules = [
+    'PhpCsFixerCustomFixers/phpdoc_no_superfluous_param' => false,
     'ordered_class_elements' => ['sort_algorithm' => 'alpha'],
 ];
 
