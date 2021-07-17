db4dfecd867f31efc9dfd82b6a07effb19acb7f3
Kei <budi03@gmail.com>
2021-07-17 20:41:32 +0700

    PolyfillTrait

---
+++ src/Traits/PolyfillTrait.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace Realodix\NextProject\Traits;
+
+use PHPUnit\Framework\Assert as PHPUnit;
+use PHPUnit\Runner\Version as PHPUnitVersion;
+use Realodix\NextProject\Support\Validator;
+
+trait PolyfillTrait
+{
+    /**
+     * Asserts that a directory does not exist.
+     *
+     * @param string $message
+     */
+    public function directoryDoesNotExist(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertDirectoryNotExists($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertDirectoryDoesNotExist($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a directory exists and is not readable.
+     *
+     * @param string $message
+     */
+    public function directoryIsNotReadable(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertDirectoryNotIsReadable($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertDirectoryIsNotReadable($this->actual, $message);
+
+        return $this;
+    }
+
+    public function directoryIsNotWritable(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertDirectoryNotIsWritable($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertDirectoryIsNotWritable($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a file does not exist.
+     *
+     * @param string $message
+     */
+    public function fileDoesNotExist(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertFileNotExists($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertFileDoesNotExist($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a file exists and is not readable.
+     *
+     * @param string $message
+     */
+    public function fileIsNotReadable(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertFileNotIsReadable($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertFileIsNotReadable($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a file exists and is not writable.
+     *
+     * @param string $message
+     */
+    public function fileIsNotWritable(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertFileNotIsWritable($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertFileIsNotWritable($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a variable is of type resource and is closed.
+     *
+     * @param string $message
+     *
+     * @return self
+     */
+    public function isClosedResource(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.3', '<')) {
+            // @codeCoverageIgnoreStart
+            if ($message === '') {
+                $message = \sprintf(
+                    'Failed asserting that %s is of type "resource (closed)"',
+                    \var_export($this->actual, true)
+                );
+            }
+
+            PHPUnit::assertTrue(Validator::isClosedResource($this->actual), $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertIsClosedResource($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a variable is not of type resource or is an open resource.
+     *
+     * @param string $message
+     *
+     * @return self
+     */
+    public function isNotClosedResource(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.3', '<')) {
+            // @codeCoverageIgnoreStart
+            if ($message === '') {
+                $message = \sprintf(
+                    'Failed asserting that %s is not of type "resource (closed)"',
+                    \var_export($this->actual, true)
+                );
+            }
+
+            PHPUnit::assertFalse(Validator::isClosedResource($this->actual), $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertIsNotClosedResource($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a file/dir exists and is not readable.
+     *
+     * @param string $message
+     */
+    public function isNotReadable(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertNotIsReadable($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertIsNotReadable($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a file/dir exists and is not writable.
+     *
+     * @param string $message
+     */
+    public function isNotWritable(string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertNotIsWritable($this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertIsNotWritable($this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a string matches a given regular expression.
+     *
+     * @param string $pattern
+     * @param string $message
+     */
+    public function matchesRegularExpression(string $pattern, string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertRegExp($pattern, $this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertMatchesRegularExpression($pattern, $this->actual, $message);
+
+        return $this;
+    }
+
+    /**
+     * Asserts that a string does not match a given regular expression.
+     *
+     * @param string $pattern
+     * @param string $message
+     */
+    public function doesNotMatchRegularExpression(string $pattern, string $message = ''): self
+    {
+        if (version_compare(PHPUnitVersion::series(), '9.1', '<')) {
+            // @codeCoverageIgnoreStart
+            PHPUnit::assertNotRegExp($pattern, $this->actual, $message);
+
+            return $this;
+            // @codeCoverageIgnoreEnd
+        }
+
+        PHPUnit::assertDoesNotMatchRegularExpression($pattern, $this->actual, $message);
+
+        return $this;
+    }
+}
