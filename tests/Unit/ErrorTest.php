41e7569499551c7a3332cb7f6eb4184661d39d87
Kei <budi03@gmail.com>
2021-07-20 23:53:08 +0700

    Update ErrorTest.php

--- tests/Unit/ErrorTest.php
+++ tests/Unit/ErrorTest.php
@@ -7,5 +7,43 @@ use Realodix\NextProject\Expect;
 
 class ErrorTest extends TestCase
 {
+    public function testDeprecationCanBeExpected(): void
+    {
+        Expect::after($this)
+            ->deprecation()
+            ->dMessage('foo')
+            ->dmMatches('/foo/');
 
+        trigger_error('foo', E_USER_DEPRECATED);
+    }
+
+    public function testNoticeCanBeExpected(): void
+    {
+        Expect::after($this)
+            ->notice()
+            ->nMessage('foo')
+            ->nmMatches('/foo/');
+
+        trigger_error('foo', E_USER_NOTICE);
+    }
+
+    public function testWarningCanBeExpected(): void
+    {
+        Expect::after($this)
+            ->warning()
+            ->wMessage('foo')
+            ->wmMatches('/foo/');
+
+        trigger_error('foo', E_USER_WARNING);
+    }
+
+    public function testErrorCanBeExpected(): void
+    {
+        Expect::after($this)
+            ->error()
+            ->erMessage('foo')
+            ->ermMatches('/foo/');
+
+        trigger_error('foo', E_USER_ERROR);
+    }
 }
