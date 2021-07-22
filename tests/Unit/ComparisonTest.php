ff0d98609f716458187b6f31a8065d15ba5d11af
Kei <budi03@gmail.com>
2021-07-22 10:49:17 +0700

    Update ComparisonTest.php

--- tests/Unit/ComparisonTest.php
+++ tests/Unit/ComparisonTest.php
@@ -7,6 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 final class ComparisonTest extends TestCase
 {
+    /**
+     * Iterable contains equal object can be asserted
+     */
     public function testContainsEquals(): void
     {
         $a = new \stdClass;
@@ -16,10 +19,31 @@ final class ComparisonTest extends TestCase
         $b->foo = 'baz';
 
         ass([$a])->containsEquals($a);
-        ass([$a])->notContainsEquals($b);
 
         try {
             ass([$a])->containsEquals($b);
+        } catch (AssertionFailedError $e) {
+            return;
+        }
+
+        $this->fail();
+    }
+
+    /**
+     * Iterable not contains equal object can be asserted
+     */
+    public function testNotContainsEquals(): void
+    {
+        $a = new \stdClass;
+        $a->foo = 'bar';
+
+        $b = new \stdClass;
+        $b->foo = 'baz';
+
+        ass([$a])->notContainsEquals($b);
+
+        try {
+            ass([$a])->notContainsEquals($a);
         } catch (AssertionFailedError $e) {
             return;
         }
