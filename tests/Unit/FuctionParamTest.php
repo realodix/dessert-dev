239716debb645035707e577e2878354b323e1fe1
Kei <budi03@gmail.com>
2021-07-23 18:46:01 +0700

    Update FuctionParamTest.php

--- tests/Unit/FuctionParamTest.php
+++ tests/Unit/FuctionParamTest.php
@@ -56,4 +56,157 @@ final class FuctionParamTest extends TestCase
         $this->expectException(InvalidActualValueException::class);
         ass('not_iterable')->containsOnlyInstancesOf('');
     }
+
+    /** @test */
+    public function countActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass('not_countable_or_iterable')->count(1);
+    }
+
+    /** @test */
+    public function notCountActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass('not_countable_or_iterable')->notCount(1);
+    }
+
+    /** @test */
+    public function classHasAttributeActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass('not_object')->classHasAttribute('#attribute');
+    }
+
+    /** @test */
+    public function classNotHasAttributeActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass('not_object')->classNotHasAttribute('!attribute');
+    }
+
+    /** @test */
+    public function classHasStaticAttributeActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass('')->classHasStaticAttribute('');
+    }
+
+    /** @test */
+    public function classNotHasStaticAttributeActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass('')->classNotHasStaticAttribute('');
+    }
+
+    /** @test */
+    public function directoryExistsActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->directoryExists();
+    }
+
+    /** @test */
+    public function directoryIsReadableActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->directoryIsReadable();
+    }
+
+    /** @test */
+    public function directoryIsWritableActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->directoryIsWritable();
+    }
+
+    /** @test */
+    public function fileEqualsActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->fileEquals('');
+    }
+
+    /** @test */
+    public function fileNotEqualsActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->fileNotEquals('');
+    }
+
+    /** @test */
+    public function fileExistsActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->fileExists();
+    }
+
+    /** @test */
+    public function fileIsReadableActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->fileIsReadable();
+    }
+
+    /** @test */
+    public function fileIsWritableActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->fileIsWritable();
+    }
+
+
+
+    /** @test */
+    public function jsonFileEqualsJsonFileActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->jsonFileEqualsJsonFile('');
+    }
+    /** @test */
+    public function jsonFileNotEqualsJsonFileActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->jsonFileNotEqualsJsonFile('');
+    }
+    /** @test */
+    public function jsonStringEqualsJsonFileActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->jsonStringEqualsJsonFile('');
+    }
+    /** @test */
+    public function jsonStringNotEqualsJsonFileActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->jsonStringNotEqualsJsonFile('');
+    }
+
+
+
+    /** @test */
+    public function jsonStringEqualsJsonStringActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->jsonStringEqualsJsonString('');
+    }
+    /** @test */
+    public function jsonStringNotEqualsJsonStringActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->jsonStringNotEqualsJsonString('');
+    }
+    /** @test */
+    public function objectHasAttributeActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->objectHasAttribute('');
+    }
+
+    /** @test */
+    public function objectNotHasAttributeActualValue(): void
+    {
+        $this->expectException(InvalidActualValueException::class);
+        ass(false)->objectNotHasAttribute('');
+    }
 }
