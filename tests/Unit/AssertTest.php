14fc56acfa8335fc64ca44231fb91903ce822c1e
Kei <budi03@gmail.com>
2021-07-23 19:09:12 +0700

    Update AssertTest.php

--- tests/Unit/AssertTest.php
+++ tests/Unit/AssertTest.php
@@ -4,6 +4,7 @@ namespace Realodix\NextProject\Test;
 
 use PHPUnit\Framework\TestCase;
 use Realodix\NextProject\Assert;
+use Realodix\NextProject\Check;
 
 final class AssertTest extends TestCase
 {
@@ -54,6 +55,10 @@ final class AssertTest extends TestCase
     public function testVariantsStaticClass(): void
     {
         Assert::that(true)
+            ->true()
+            ->notFalse();
+
+        Check::that(true)
             ->true()
             ->notFalse();
     }
