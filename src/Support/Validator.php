ee56845d6480ed5be455634f574d245e0f6d3f4e
Kei <budi03@gmail.com>
2021-07-25 00:13:23 +0700

    Update Validator.php

--- src/Support/Validator.php
+++ src/Support/Validator.php
@@ -8,6 +8,78 @@ namespace Realodix\NextProject\Support;
 final class Validator
 {
     /**
+     * Determines whether a variable represents a closed resource.
+     *
+     * @param mixed $value The variable being type checked
+     *
+     * @return bool
+     */
+    public static function isClosedResource($value): bool
+    {
+        // Introduced in PHP 8 as alternative for get_debug_type(). Thanks
+        // symfony/polyfill-php80, it works for PHP < 7.2
+        if (get_debug_type($value) === 'resource (closed)') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if a string is valid JSON
+     *
+     * @param string $value
+     *
+     * @return bool
+     */
+    public static function isJson(string $value): bool
+    {
+        if ($value === '') {
+            return false;
+        }
+
+        json_decode($value);
+
+        if (json_last_error()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if a string is valid XML
+     * - https://stackoverflow.com/a/31240779/2732184
+     * - https://github.com/mtvbrianking/laravel-xml/blob/master/src/Support/XmlValidator.php
+     *
+     * @param string $xml
+     *
+     * @return bool
+     */
+    public static function isXml(string $xml): bool
+    {
+        $content = trim($xml);
+        if (empty($content)) {
+            return false;
+        }
+
+        // ignore html
+        if (false !== stripos($content, '<!DOCTYPE html>')) {
+            return false;
+        }
+
+        libxml_use_internal_errors(true);
+
+        simplexml_load_string($content);
+
+        $errors = libxml_get_errors();
+
+        libxml_clear_errors();
+
+        return empty($errors);
+    }
+
+    /**
      * Determines whether the actual value given is valid or invalid
      *
      * @param mixed  $actualValue
@@ -115,77 +187,5 @@ final class Validator
 
                 return $expectedValue;
         }
-    }
-
-    /**
-     * Determines whether a variable represents a closed resource.
-     *
-     * @param mixed $value The variable being type checked
-     *
-     * @return bool
-     */
-    public static function isClosedResource($value): bool
-    {
-        // Introduced in PHP 8 as alternative for get_debug_type(). Thanks
-        // symfony/polyfill-php80, it works for PHP < 7.2
-        if (get_debug_type($value) === 'resource (closed)') {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Check if a string is valid JSON
-     *
-     * @param string $value
-     *
-     * @return bool
-     */
-    public static function isJson(string $value): bool
-    {
-        if ($value === '') {
-            return false;
-        }
-
-        json_decode($value);
-
-        if (json_last_error()) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Check if a string is valid XML
-     * - https://stackoverflow.com/a/31240779/2732184
-     * - https://github.com/mtvbrianking/laravel-xml/blob/master/src/Support/XmlValidator.php
-     *
-     * @param string $xml
-     *
-     * @return bool
-     */
-    public static function isXml(string $xml): bool
-    {
-        $content = trim($xml);
-        if (empty($content)) {
-            return false;
-        }
-
-        // ignore html
-        if (false !== stripos($content, '<!DOCTYPE html>')) {
-            return false;
-        }
-
-        libxml_use_internal_errors(true);
-
-        simplexml_load_string($content);
-
-        $errors = libxml_get_errors();
-
-        libxml_clear_errors();
-
-        return empty($errors);
     }
 }
