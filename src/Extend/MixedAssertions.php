7e563276979817d40b1e1d5093bf18bbc874f18c
Kei <budi03@gmail.com>
2021-07-25 10:56:27 +0700

    Create MixedAssertions.php

---
+++ src/Extend/MixedAssertions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Realodix\NextProject\Extend;
+
+use PHPUnit\Framework\Assert;
+use Realodix\NextProject\Support\Validator;
+
+/**
+ * Mixed assertions.
+ *
+ * @internal
+ */
+final class MixedAssertions
+{
+
+}
