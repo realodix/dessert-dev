b9ce04b6a1e521b9181cfd514ff542e694ff4dcc
Kei <budi03@gmail.com>
2021-09-06 16:04:00 +0700

    IsTypeTest

--- tests/Unit/PHPUnitTest.php
+++ tests/Unit/PHPUnitTest.php
@@ -6,6 +6,9 @@ use PHPUnit\Framework\AssertionFailedError;
 use PHPUnit\Framework\TestCase;
 use Realodix\NextProject\Test\Fixtures\ObjectEquals\ValueObject;
 
+// Coba dibuat, ditemukan pada test Dir
+// $this->expectException(AssertionFailedError::class);
+
 final class PHPUnitTest extends TestCase
 {
     public function testClassHasAttribute(): void
@@ -263,6 +266,120 @@ final class PHPUnitTest extends TestCase
         ass($testClass)
             ->instanceOf('DateTime')
             ->notInstanceOf('DateTimeZone');
+    }
+
+    /** @test */
+    public function isArray(): void
+    {
+        ass([1, 2, 3])->isArray();
+        ass(false)->isNotArray();
+    }
+
+    /** @test */
+    public function isBool(): void
+    {
+        ass(false)->isBool();
+        ass([1, 2, 3])->isNotBool();
+    }
+
+    /** @test */
+    public function isCallable(): void
+    {
+        ass(function (): void {})->isCallable();
+
+        ass(false)->isNotCallable();
+    }
+
+    /** @test */
+    public function isClosedResource(): void
+    {
+        $resource = fopen(__FILE__, 'r');
+        fclose($resource);
+
+        ass($resource)->isClosedResource();
+
+        ass(null)->isNotClosedResource();
+        ass(['array'])->isNotClosedResource();
+        ass(new \stdClass)->isNotClosedResource();
+
+        // isClosedResource() test for code coverage
+        ass(\Realodix\NextProject\Support\Validator::isClosedResource($resource))
+            ->true();
+        ass(\Realodix\NextProject\Support\Validator::isClosedResource(null))
+            ->false();
+    }
+
+    /** @test */
+    public function isFloat(): void
+    {
+        ass(1.5)->isFloat();
+        ass(1)->isNotFloat();
+    }
+
+    /** @test */
+    public function isInt(): void
+    {
+        ass(5)->isInt();
+        ass(1.5)->isNotInt();
+    }
+
+    /** @test */
+    public function isIterable(): void
+    {
+        ass([])->isIterable();
+        ass(null)->isNotIterable();
+    }
+
+    /** @test */
+    public function isNumeric(): void
+    {
+        ass('1.5')->isNumeric();
+        ass('foo bar')->isNotNumeric();
+    }
+
+    /** @test */
+    public function isObject(): void
+    {
+        ass(new \stdClass)->isObject();
+        ass(false)->isNotObject();
+    }
+
+    /** @test */
+    public function isResource(): void
+    {
+        ass(fopen(__FILE__, 'r'))->isResource();
+        ass(false)->isNotResource();
+    }
+
+    /** @test */
+    public function isScalar(): void
+    {
+        ass('foo bar')->isScalar();
+        ass([1, 2, 3])->isNotScalar();
+    }
+
+    /** @test */
+    public function isString(): void
+    {
+        ass('foo bar')->isString();
+        ass(false)->isNotString();
+    }
+
+    public function testIsReadable(): void
+    {
+        ass(TEST_FILES_PATH.'string_foobar.txt')->isReadable();
+
+        $path = __DIR__.\DIRECTORY_SEPARATOR.'NotExisting.php';
+        ass($path)->isNotReadable();
+    }
+
+    public function testIsWritable(): void
+    {
+        $path = TEST_FILES_PATH.'string_foobar.txt';
+        ass($path)->isWritable();
+
+        $path = __DIR__.\DIRECTORY_SEPARATOR.'NotExisting'.\DIRECTORY_SEPARATOR;
+        ass($path)->isNotWritable();
     }
 
     /**
