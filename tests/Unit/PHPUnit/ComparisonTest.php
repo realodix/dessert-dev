b330cb7efb49ac979488305f5140c6f755ce93a1
Kei <budi03@gmail.com>
2021-09-06 16:33:23 +0700

    testXmlFileEqualsXmlFile

--- tests/Unit/PHPUnit/ComparisonTest.php
+++ tests/Unit/PHPUnit/ComparisonTest.php
@@ -50,31 +50,4 @@ final class ComparisonTest extends TestCase
 
         $this->fail();
     }
-
-    public function testXmlFileEqualsXmlFile(): void
-    {
-        $actual = TEST_FILES_PATH.'xml_foo.xml';
-        $expected = TEST_FILES_PATH.'xml_bar.xml';
-
-        ass($actual)
-            ->xmlFileToFile($actual)
-            ->xmlFileNotToFile($expected);
-    }
-
-    public function testXmlStringEqualsXmlFile(): void
-    {
-        $xmlFoo = TEST_FILES_PATH.'xml_foo.xml';
-        $xmlBar = TEST_FILES_PATH.'xml_bar.xml';
-
-        ass('<foo/>')
-            ->xmlStringToFile($xmlFoo)
-            ->xmlStringNotToFile($xmlBar);
-    }
-
-    public function testXmlStringEqualsXmlString(): void
-    {
-        ass('<foo/>')
-            ->xmlStringToString('<foo/>')
-            ->xmlStringNotToString('<bar/>');
-    }
 }
--- tests/Unit/PHPUnitTest.php
+++ tests/Unit/PHPUnitTest.php
@@ -500,6 +500,33 @@ final class PHPUnitTest extends TestCase
         ass(true)->notFalse();
     }
 
+    public function testXmlFileEqualsXmlFile(): void
+    {
+        $actual = TEST_FILES_PATH.'xml_foo.xml';
+        $expected = TEST_FILES_PATH.'xml_bar.xml';
+
+        ass($actual)
+            ->xmlFileToFile($actual)
+            ->xmlFileNotToFile($expected);
+    }
+
+    public function testXmlStringEqualsXmlFile(): void
+    {
+        $xmlFoo = TEST_FILES_PATH.'xml_foo.xml';
+        $xmlBar = TEST_FILES_PATH.'xml_bar.xml';
+
+        ass('<foo/>')
+            ->xmlStringToFile($xmlFoo)
+            ->xmlStringNotToFile($xmlBar);
+    }
+
+    public function testXmlStringEqualsXmlString(): void
+    {
+        ass('<foo/>')
+            ->xmlStringToString('<foo/>')
+            ->xmlStringNotToString('<bar/>');
+    }
+
     /**
      * Two objects can be asserted to be equal using comparison method.
      */
