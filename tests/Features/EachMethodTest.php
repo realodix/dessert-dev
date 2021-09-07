f840a6ef61ed3fa362927e0834c09a8349b143c0
Kei <budi03@gmail.com>
2021-09-07 16:49:00 +0700

    Update EachMethodTest.php

--- tests/Features/EachMethodTest.php
+++ tests/Features/EachMethodTest.php
@@ -3,6 +3,7 @@
 namespace Realodix\NextProject\Test\Features;
 
 use PHPUnit\Framework\TestCase;
+use Realodix\NextProject\Assertion;
 
 final class EachMethodTest extends TestCase
 {
@@ -35,5 +36,93 @@ final class EachMethodTest extends TestCase
         );
 
         ass('Foobar')->each()->same('Foobar');
+    }
+
+    /**
+     * Chains expectations on each item
+     */
+    public function testChainsExpectations(): void
+    {
+        ass([1, 1, 1])
+            ->each()
+            ->isInt()
+            ->equals(1);
+
+        ass(static::getCount())->same(6); // + 1 assertion
+
+        ass([2, 2, 2])
+            ->each
+            ->isInt
+            ->equals(2);
+
+        ass(static::getCount())->same(13);
+    }
+
+    /**
+     * Opposite expectations on each item
+     */
+    // public function testOpposite(): void
+    // {
+    //     ass([1, 2, 3])
+    //         ->each()
+    //         ->not()
+    //         ->equals(4);
+
+    //     ass(static::getCount())->same(3);
+
+    //     ass([1, 2, 3])
+    //         ->each()
+    //         ->not->isString;
+
+    //     ass(static::getCount())->same(7);
+    // }
+
+    /**
+     * Chained opposite and non-opposite expectations
+     */
+    // public function testChainedOppositeAndNonOpposite(): void
+    // {
+    //     ass([1, 2, 3])
+    //         ->each()
+    //         ->not()
+    //         ->equals(4)
+    //         ->isInt();
+
+    //     ass(static::getCount())->same(6);
+    // }
+
+    /**
+     * Can add expectations via "and"
+     */
+    // public function testCanAddExpectationsViaAnd(): void
+    // {
+    //     ass([1, 2, 3])
+    //         ->each()
+    //         ->isInt // + 3
+    //         ->and([4, 5, 6])
+    //         ->each
+    //         ->lessThan(7) // + 3
+    //         ->not
+    //         ->lessThan(3)
+    //         ->greaterThan(3) // + 3
+    //         ->and('Hello World')
+    //         ->isString // + 1
+    //         ->equals('Hello World'); // + 1
+
+    //     ass(static::getCount())->same(14);
+    // }
+
+    /**
+     * Accepts callables
+     */
+    public function testCallables(): void
+    {
+        ass([1, 2, 3])->each(function ($number) {
+            ass($number)->instanceOf(Assertion::class);
+            ass($number->value)->isInt();
+            $number->isInt->not->isString;
+        });
+
+        ass(static::getCount())->same(12);
     }
 }
