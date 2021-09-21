a54ee6e476c502c0064a19ba98721d0aaa3d3e6a
Kei <budi03@gmail.com>
2021-09-22 03:40:08 +0700

    Update ShortNameTest.php

--- tests/Unit/Custom/ShortNameTest.php
+++ tests/Unit/Custom/ShortNameTest.php
@@ -76,24 +76,6 @@ final class ShortNameTest extends TestCase
             ->jsonStringNotToString(json_encode(['foo' => 'baz']));
     }
 
-    public function testGreaterThan(): void
-    {
-        ass(2)->greater(1)
-              ->greaterOrEqual(2);
-
-        ass(2)->isAbove(1)
-              ->isAtLeast(2);
-    }
-
-    public function testLessThan(): void
-    {
-        ass(1)->less(2)
-              ->lessOrEqual(1);
-
-        ass(1)->isBelow(2)
-              ->isAtMost(1);
-    }
-
     public function testMatch(): void
     {
         ass('foobar')
