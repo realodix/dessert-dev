48a2bffa5ba2c230c5e0cd68fc6e4ce9420739b1
Realodix <budi03@gmail.com>
2022-06-22 00:52:37 +0700

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,12 +7,12 @@ use Realodix\CsConfig\Rules\Realodix;
 $finder = Finder::base(__DIR__);
 $addOrOverrideRules = [
     // Realodix
-    'phpdoc_annotation_without_dot' => false,
     'binary_operator_spaces' => [
         'operators' => [
             '=>' => 'align_single_space',
             '=' => 'single_space']
     ],
+    'phpdoc_annotation_without_dot' => false,
 ];
 
 return Config::create(new Realodix($addOrOverrideRules))
