0cbff2bb7b73d1210a667e186e1d633b19d991f5
Realodix <budi03@gmail.com>
2022-08-22 03:17:29 +0700

    fileNotEqualsStringIgnoringCase()

--- src/Traits/CustomTrait.php
+++ src/Traits/CustomTrait.php
@@ -96,11 +96,11 @@ trait CustomTrait
 
     public function fileNotEqualsStringIgnoringCase(string $expectedString, string $message = ''): self
     {
-        $actual = Validator::actualValue($this->actual, 'string');
-        Assert::assertFileExists($actual, $message);
+        Validator::actualValue($this->actual, 'string');
+        Assert::assertFileExists($this->actual, $message);
 
         $constraint = new LogicalNot(new IsEqualIgnoringCase($expectedString));
-        Assert::assertThat(file_get_contents($actual), $constraint, $message);
+        Assert::assertThat(file_get_contents($this->actual), $constraint, $message);
 
         return $this;
     }
